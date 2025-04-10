07bb31b5b0d522d7d121dee420b89b5f593d6480
Zain <zain2.malik@live.uwe.ac.uk>
2025-04-10 23:28:18 +0100

    Lovable Change

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Lock } from "lucide-react";
 import Navbar from "@/components/Navbar";
